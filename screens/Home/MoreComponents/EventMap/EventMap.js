afc60ab073ece4785fd4111d2fa97920480c4688
BalintHompot <hompot.balint@gmail.com>
2021-11-03 21:36:50 +0100

    maps first tests

--- screens/Home/MoreComponents/EventMap/EventMap.js
+++ screens/Home/MoreComponents/EventMap/EventMap.js
@@ -1,39 +1,42 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import MapView, { Marker, Callout, UrlTile } from 'react-native-maps'
 import { createStackNavigator } from '@react-navigation/stack'
+import SlidingPanel from 'react-native-sliding-up-down-panels';
+import styleconstants from "../../../../shared/styles/styleconstants";
+const { width, height } = Dimensions.get('window');
 
 const EventMap = (props) => {
 
     return (
-        <View>
-            <MapView
-                provider="google"
-                style={styles.map}
-                showsPointsOfInterest={false}
-                showsUserLocation={true}
-                showsMyLocationButton={true}
-                // cacheEnabled={true}
-                region={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.001,
-                    longitudeDelta: 0.001
-                }}
-                // minZoomLevel={16}
-                maxZoomLevel={20}
+        <View style={styles.container}>
+            <View style={styles.bodyViewStyle}>
+                <MapView
+                    provider="google"
+                    initialRegion={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
+            </View>
 
-            >
-
-                {/* <Marker
-            draggable
-            coordinate={startingCoordinate}
-            onDragEnd={e => console.log(e.nativeEvent.coordinate)}
-          >
-            <PreviewMapMarker amount="x" />
-          </Marker> */}
-            </MapView>
+            <SlidingPanel
+                headerLayoutHeight={40}
+                headerLayout={() =>
+                    <View style={styles.headerLayoutStyle}>
+                        <Text style={styles.commonTextStyle}>My Awesome sliding panel</Text>
+                    </View>
+                }
+                slidingPanelLayout={() =>
+                    <View style={styles.slidingPanelLayoutStyle}>
+                        <Text style={styles.commonTextStyle}>The best thing about me is you</Text>
+                    </View>
+                }
+            />
         </View>
+
 
     );
 
@@ -48,5 +51,29 @@ const styles = StyleSheet.create({
     },
     map: {
         ...StyleSheet.absoluteFillObject
-    }
+    },
+
+    bodyViewStyle: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerLayoutStyle: {
+        width,
+        height: 100,
+        backgroundColor: styleconstants.bggorange,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    slidingPanelLayoutStyle: {
+        width,
+        height,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    commonTextStyle: {
+        color: 'white',
+        fontSize: 18,
+    },
 })
